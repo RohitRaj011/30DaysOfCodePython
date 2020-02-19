52b49a2c34e20fa3ee7910f8a755f6455c268405
RohitRaj011 <rohit011@protonmail.com>
2020-02-19 19:42:43 +0530

    day 10

--- 30 Days of Code/day_10_binary_numbers.py
+++ 30 Days of Code/day_10_binary_numbers.py
@@ -1,1 +1,12 @@
-
+n = int(input())
+count = 0
+max_count = 0
+binary = bin(n).replace("0b", "")
+for i in binary:
+    if i == '1':
+        count += 1
+        if max_count < count:
+            max_count = count
+    else:
+        count = 0
+print(max_count)
